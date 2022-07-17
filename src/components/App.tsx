f9c7f9604b164b401f884b59ca50008bdca4d7f3
Jonathan Ullrich <ullrich.jonathan@googlemail.com>
2022-07-17 23:34:37 +0200

    changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,43 @@
-import { useState } from 'react';
-import Board from './Board';
+import { useState } from 'react'
+
+import Synagogue from './Synagogue';
 
 function App() {
 
-  return (
-    <>
-      <h1>Golem Synagoge Randomizer</h1>
-      <Board playerCount={2}/>
-    </>
-  )
+  const [playerCount, setPlayerCount] = useState(0);
+
+  switch (playerCount) {
+    case 2:
+      return (
+        <>
+          <h1>Golem Synagogue Randomizer</h1>
+          <Synagogue playerCount={2} />
+        </>
+      )
+    case 3:
+      return (
+        <>
+          <h1>Golem Synagogue Randomizer</h1>
+          <Synagogue playerCount={3} />
+        </>
+      )
+    case 4:
+      return (
+        <>
+          <h1>Golem Synagogue Randomizer</h1>
+          <Synagogue playerCount={4} />
+        </>
+      )
+    default: 
+      return (
+        <>
+          <h1>Golem Synagogue Randomizer</h1>
+          <button onClick={ () => setPlayerCount(2) }>2 Player</button>
+          <button onClick={ () => setPlayerCount(3) }>3 Player</button>
+          <button onClick={ () => setPlayerCount(4) }>4 Player</button>
+        </>
+      )
+    }
 }
 
 export default App;
---
+++ src/components/Synagogue.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+
+type BoardProps = {
+    playerCount: number; 
+}
+
+function Board(props: BoardProps) {
+
+    const [board, setBoard] = useState<string[][]>([[], [], [], [], []]);
+
+    useEffect(() => {
+        let marbles: string[];
+        if (props.playerCount === 2) {
+            marbles = ['white', 'white', 'white', 'black', 'red', 'red', 'blue', 'blue', 'yellow', 'yellow'];
+        } else if (props.playerCount === 3) {
+            marbles = ['white', 'white', 'white', 'white', 'white', 'black', 'red', 'red', 'blue', 'blue', 'yellow', 'yellow'];
+        } else {
+            marbles = ['white', 'white', 'white', 'white', 'black', 'red', 'red', 'red', 'blue', 'blue', 'blue', 'yellow', 'yellow', 'yellow'];
+        }
+        let arr: string[][] = Array.from([[], [], [], [], []]);
+        for (let marble of marbles) {
+            let slot = Math.floor(Math.random() * 5);
+            arr[slot].push(marble);
+        }
+        setBoard(arr);
+    }, []);
+
+    const removeMarble = (i: number, j: number) => {
+        let arr = Array.from(board);
+        arr[i].splice(j, 1);
+        setBoard(arr);
+    }
+
+    return (
+        <div className='synagogue'>
+            <div className='board'>
+            {board.map((column, i) => {
+                return (
+                    <div className='column' key={`${i}`}>
+                        {column.map((color, j) => {
+                            return (
+                                <div 
+                                    className={`marble ${color}`}
+                                    onClick={() => removeMarble(i, j)}
+                                    key={`${i}${j}`}>
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
+            </div>
+            <div className='total'>
+            {board.map((column, i) => {
+                return (
+                    <div key={i+100}>{column.length}</div>
+                );
+            })}
+            </div>
+        </div>
+    )
+}
+
+export default Board;
